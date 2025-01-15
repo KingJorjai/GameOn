4b07fecf31bc5b2df4da3803c8cf20b874cf6738
Jorjai <jorge.are.fdz@gmail.com>
2025-01-15 17:50:42 +0100

    Improve activity detection + Minor tweaks
    - Fix duplicate messages.
    - Now supports starting an activity without ending the previous one.
    - Improved embed readability.

--- src/main/java/net/jorjai/modules/PingActivityStartModule/PingActivityStartHandler.java
+++ src/main/java/net/jorjai/modules/PingActivityStartModule/PingActivityStartHandler.java
@@ -4,14 +4,19 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Query;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.events.user.UserActivityEndEvent;
 import net.dv8tion.jda.api.events.user.UserActivityStartEvent;
 import net.jorjai.bot.Bot;
+import org.slf4j.LoggerFactory;
 
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class PingActivityStartHandler {
+
+    private static Map<Long,String> activeUsers = Collections.synchronizedMap(new HashMap<>());
+
     public static boolean registerPing (long target_id, String game, long subscriber_id) {
         EntityManager em = Bot.getEmf().createEntityManager();
         EntityTransaction et = em.getTransaction();
@@ -79,38 +84,89 @@ public class PingActivityStartHandler {
     }
 
     public static void onUserActivityStart(UserActivityStartEvent event){
-        // Return if activity is not playing
-//        if (event.getNewActivity().getType() != Activity.ActivityType.PLAYING) {
-//            return;
-//        }
 
-        // Get the target id and game
-        long target_id = event.getUser().getIdLong();
-        String game = Objects.requireNonNull(event.getNewActivity()).getName();
-        String target_username = event.getUser().getName();
-        String activity_type = event.getNewActivity().getType().toString().toLowerCase();
+        long targetId = event.getUser().getIdLong();
+        Activity.ActivityType activityType = event.getNewActivity().getType();
+        String game = event.getNewActivity().getName();
+
+        // Checks
+        if (
+            (activeUsers.containsKey(targetId) && game.equals(activeUsers.get(targetId))) || // Avoid multiple calls from each server
+            activityType == Activity.ActivityType.CUSTOM_STATUS                             // Ignore custom status
+        ) {
+            LoggerFactory.getLogger(PingActivityStartHandler.class).debug("Ignoring activity start event for user {}", event.getUser().getName());
+            return;
+        }
+
+        // Add the user to the active users list
+        activeUsers.put(targetId, game);
+        LoggerFactory.getLogger(PingActivityStartHandler.class).debug("Added user {} to the active users list with activity {}", event.getUser().getName(), game);
+
+        // Retrieve the game and the target username
+        String targetUsername = event.getUser().getName();
+        String action = activityTypeToActionString(activityType);
 
 
-        // create the embed
+        // Create the embed
         MessageEmbed embed = new EmbedBuilder()
                 .setColor(0x69ffc9)
-                .setAuthor(target_username + " is now " + activity_type + " " + game, null, event.getUser().getAvatarUrl())
+                .setAuthor(targetUsername + " is now " + action + " " + game, null, event.getUser().getAvatarUrl())
                 .build();
 
-        // Look for subscribers and send them the message
+        // Look for subscribers to this game or all
         EntityManager em = Bot.getEmf().createEntityManager();
-
         Query query = em.createQuery("SELECT p FROM PingActivityStartData p WHERE p.target_id = :targetId AND (p.game = :game OR p.game = '*')");
-        query.setParameter("targetId", target_id);
+        query.setParameter("targetId", targetId);
         query.setParameter("game", game);
-
         List<PingActivityStartData> result = query.getResultList();
         em.close();
 
+        // Send the message to the subscribers
         for (PingActivityStartData data : result) {
             event.getJDA().getUserById(data.getSubscriber_id()).openPrivateChannel().queue(privateChannel -> {
                 privateChannel.sendMessageEmbeds(embed).queue();
+                LoggerFactory.getLogger(PingActivityStartHandler.class).debug("Sent message to user " + data.getSubscriber_id());
             });
         }
+    }
+
+    public static void onUserActivityEnd(UserActivityEndEvent event) {
+        long targetId = event.getUser().getIdLong();
+
+        if(activeUsers.containsKey(targetId) && activeUsers.get(targetId).equals(event.getOldActivity().getName())){
+            LoggerFactory.getLogger(PingActivityStartHandler.class).debug("Removed user {} from the active users list", event.getUser().getName());
+            activeUsers.remove(targetId);
+        }
+    }
+
+    /**
+     * Converts an activity type to a string that describes the action
+     *
+     * @param activityType The activity type
+     * @return The string that describes the action
+     */
+    private static String activityTypeToActionString(Activity.ActivityType activityType) {
+        String action = "doing something";
+        switch (activityType){
+            case COMPETING -> {
+                action = "competing in";
+            }
+            case CUSTOM_STATUS -> {
+                action = "using a custom status";
+            }
+            case LISTENING -> {
+                action = "listening to";
+            }
+            case PLAYING -> {
+                action = "playing";
+            }
+            case STREAMING -> {
+                action = "streaming";
+            }
+            case WATCHING -> {
+                action = "watching";
+            }
+        }
+        return action;
     }
 }
